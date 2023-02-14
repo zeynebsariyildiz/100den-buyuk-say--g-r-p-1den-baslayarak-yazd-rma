864b1fcf1130e1fb60f0db02f1cea8fe7f0c6b48
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-02-14 13:43:24 +0300

    if else ekledim

--- Program.cs
+++ Program.cs
@@ -10,15 +10,28 @@ namespace _100den_buyuk_sayı_gırıp_1den_baslayarak_yazdırma
     {
         static void Main(string[] args)
         {
-             
+
             Console.WriteLine("Lütfen 100 den Büyük Bir Sayı Giriniz");
             int sayi = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < sayi; i++)
+            if (sayi > 100)
             {
-                Console.WriteLine(i);
-                
+                for (int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
             }
+            else 
+            {
+                Console.WriteLine("Hatalı Sayı Girdiniz");
+                Console.ReadLine();
+
+            }
+
             Console.ReadLine();
+            
             // 100 den küçük bir sayı girdiğimde döngüye girip yazdırıyor, uyarı vermesi gerekirdi
         }
     }
